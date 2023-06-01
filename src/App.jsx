3336198ae2e665b08d7132bad6c5304c2ab1f2f3
rainbow1223 <greatwisdom1223@gmail.com>
2023-06-01 11:11:46 +0200

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css';
 
+import Sidebar from './components/sidebar/Sidebar';
+
 function App() {
   return (
     <div className="App">
+      {/* <Sidebar /> */}
       <h1>Test website for Headshot chatbot</h1>
       <h2>Automate headshot booking process</h2>
+      <img src="1.jpg" alt="" />
     </div>
   );
 }
